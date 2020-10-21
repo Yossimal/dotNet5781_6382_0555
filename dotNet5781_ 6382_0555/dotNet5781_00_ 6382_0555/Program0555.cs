cd28df7cd777306f2473388b15de64cee05d685f
Yosef Malka <yosef.malka@gmail.com>
2020-10-21 13:55:37 +0300

    Initial code + Welcome0555

---
+++ dotNet5781_ 6382_0555/dotNet5781_00_ 6382_0555/Program0555.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00__6382_0555
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome0555();
+
+            Console.ReadKey();
+        }
+
+        private static void Welcome0555()
+        {
+            Console.WriteLine("Enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0} , welcome to my first console application", name);
+        }
+
+        static partial void Welcome6382();
+    }
+}
+
