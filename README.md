5b71c2eaa5e29f89797a1d110bd7d424e84ff53d
Aaron Kremer <kremerad@gmail.com>
2020-11-02 12:46:17 +0200

    finished bus object

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-# dotNet5781_00_6382_0555
+<<<<<<< HEAD
 Targil 0 - Yosef Malka, Aaron Kremer
 # dotNet5781_00_6382_0555
+=======
+# dotNet5781_00_6382_0555
 Hello World
+>>>>>>> db916b1c4ea4127d1d33d3d52a188adc2d1b8090
---
+++ dotNet5781_ 6382_0555/dotNet5781_01_ 6382_0555/Bus.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace dotNet5781_01__6382_0555
+{
+    class Bus
+    {
+        private int licenseNumber;
+        private DateTime startWorkingDate;
+        private int mileageAfterCare;
+        private int mileageCounter;
+        private int kmAfterRefueling;
+        private DateTime lastCareDate;
+
+        public Bus(int licenseNumber, DateTime startWorkingDate)
+        {
+            this.licenseNumber = licenseNumber;
+            this.startWorkingDate = startWorkingDate;
+            this.mileageAfterCare = 0;
+            this.kmAfterRefueling = 0;
+            this.lastCareDate = DateTime.Now;
+            Random rand = new Random(DateTime.Now.Millisecond);
+            this.mileageCounter = rand.Next(0, 20000);
+        }
+
+        public static int MaxKmBeforeCare { get => 20000; }
+        public static int MaxKmAfterRefueling { get => 1200; }
+        public bool IsDanger
+        {
+            get
+            {
+                DateTime zeroTime = new DateTime(1, 1, 1);
+
+                TimeSpan span = lastCareDate - DateTime.Now;
+
+                int yearsAfterCare = (zeroTime + span).Year - 1;
+                return MileageAfterCare > MaxKmBeforeCare && yearsAfterCare >= 1;
+            }
+        }
+
+        public int LicenseNumber { get => licenseNumber; }
+        public DateTime StartWorkingDate { get => startWorkingDate; }
+        public int MileageAfterCare { get => mileageAfterCare; }
+        public int MileageCounter { get => mileageCounter; }
+        public int KmAfterRefueling { get => kmAfterRefueling; }
+        public DateTime LastCareDate { get => lastCareDate; }
+        public void Refuel() { kmAfterRefueling = 0; }
+        public void TakeCare()
+        {
+            mileageAfterCare = 0;
+            lastCareDate = DateTime.Now;
+        }
+        public bool CanDrive(int distance)
+        {
+            return !this.IsDanger && distance > (MaxKmAfterRefueling - kmAfterRefueling);
+        }
+        public bool Drive(int distance)
+        {
+            if (!CanDrive(distance))
+            {
+                return false;
+            }
+            kmAfterRefueling += distance;
+            mileageCounter += distance;
+            mileageAfterCare += distance;
+            return true;
+        }
+
+    }
+}
